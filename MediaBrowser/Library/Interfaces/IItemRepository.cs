95eefc203e85e21f079a31cab313f36dce31e63e
Sam Saffron <sam.saffron@gmail.com>
2009-12-07 08:09:58 +1100

    Added ignore file, fixed http://community.mediabrowser.tv/permalinks/195/movie-overview-title-repeats

---
+++ MediaBrowser/Library/Interfaces/IItemRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediaBrowser.Library.Entities;
+using MediaBrowser.Library.Interfaces;
+
+namespace MediaBrowser.Library
+{
+    public interface IItemRepository
+    {
+        IEnumerable<IMetadataProvider> RetrieveProviders(Guid guid);
+        void SaveProviders(Guid guid, IEnumerable<IMetadataProvider> providers);
+
+        void SaveItem(BaseItem item);
+        BaseItem RetrieveItem(Guid name);
+        void SaveChildren(Guid ownerName, IEnumerable<Guid> children);
+        IEnumerable<Guid> RetrieveChildren(Guid id);
+
+        PlaybackStatus RetrievePlayState(Guid id);
+        DisplayPreferences RetrieveDisplayPreferences(Guid id);
+
+        void SavePlayState( PlaybackStatus playState);
+        void SaveDisplayPreferences(DisplayPreferences prefs);
+
+
+        void CleanCache();
+
+        bool ClearEntireCache();
+        
+    }
+}
