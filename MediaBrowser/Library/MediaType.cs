c8beb24f19d7e340da1a61aa1f16fa0a21c4e0ed
Robert Hobbins <throgmorton@localhost>
2009-12-09 18:17:29 -0500

    Trunk: - Fixed Network Images from not showing (http://community.mediabrowser.tv/topics/829) - Added Recently Unwatched to Vanilla (http://community.mediabrowser.tv/topics/859) - Applied ebr's fix concerning extenders to vanilla (fingers crossed if it works) - Added watched icon to list view - Reorganized Vanilla Source Code

---
+++ MediaBrowser/Library/MediaType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MediaBrowser.Library {
+    public enum MediaType { 
+        Unknown, 
+        Mpg, 
+        Avi, 
+        Mkv,
+        Mp4,
+        DVD, 
+        BluRay, 
+        HDDVD,
+        TS,
+        ISO,
+        PlayList, 
+        DVRMS
+    }
+}
