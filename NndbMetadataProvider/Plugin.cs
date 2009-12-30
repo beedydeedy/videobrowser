6afa7375ee5965af46f59f78c1a7a3a82dcbc5c8
admin <admin@localhost>
2009-12-30 14:17:52 -0800

    - Dynamic refresh when files are updated (moved, deleted, renamed)

---
+++ NndbMetadataProvider/Plugin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MediaBrowser.Library.Plugins;
+using MediaBrowser.Library.Logging;
+using MediaBrowser.Library;
+
+namespace NndbMetadataProvider {
+    class Plugin : BasePlugin {
+
+        public override void Init(Kernel kernel) {
+            kernel.MetadataProviderFactories.Add(MetadataProviderFactory.Get<NndbPeopleProvider>());
+        }
+
+        public override string Name {
+            get { return "Nndb image provider"; }
+        }
+
+        public override string Description {
+            get { return "Downloads actor and director images from nndb.com"; }
+        }
+    }
+}
