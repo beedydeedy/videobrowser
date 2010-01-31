056886a188505b7e2bf56cab34b8250daf3777bb
Eric <ebr@localhost>
2010-01-30 20:17:55 -0500

    Backed out changeset: c23ab78f4a5d  (version bump makes everything blow chow)

---
+++ MediaBrowser/Library/Factories/LibraryImageFactory.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MediaBrowser.Library.ImageManagement;
+using System.Diagnostics;
+using MediaBrowser.Library.Logging;
+using MediaBrowser.Library.Entities;
+
+namespace MediaBrowser.Library.Factories {
+
+    public delegate LibraryImage ImageResolver(string path, bool canBeProcessed, BaseItem item); 
+
+    public class LibraryImageFactory {
+        public static LibraryImageFactory Instance = new LibraryImageFactory();
+
+        private LibraryImageFactory() {
+        }
+
+        Dictionary<string, LibraryImage> cache = new Dictionary<string, LibraryImage>();
+
+        public void ClearCache() {
+            lock (cache) {
+                cache.Clear();
+            }
+        }
+
+        public void ClearCache(string path) {
+            lock (cache) {
+                if (cache.ContainsKey(path)) {
+                    cache.Remove(path);
+                }
+            }
+        }
+
+        public LibraryImage GetImage(string path)
+        {
+            return GetImage(path, false, null);
+        }
+
+        public LibraryImage GetImage(string path, bool canBeProcessed, BaseItem item) {
+            LibraryImage image = null;
+            bool cached = false;
+
+            lock(cache){
+                cached = cache.TryGetValue(path, out image);
+            }
+
+            if (!cached && image == null) {
+                try {
+
+                    foreach (var resolver in Kernel.Instance.ImageResolvers) {
+                        image = resolver(path, canBeProcessed, item);
+                        if (image != null) break;
+                    }
+
+                    if (image == null) {
+                        if (canBeProcessed)
+                            image = new FilesystemProcessedImage(item);
+                        else
+                            image = new FilesystemImage();
+                    }
+
+                    image.Path = path;
+                    image.Init();
+
+                    // this will trigger a download and a resize
+                    // image.EnsureImageSizeInitialized();
+                } catch (Exception ex) {
+                    Logger.ReportException("Failed to load image: " + path + " ", ex);
+                    image = null;
+                }
+            }
+
+            lock (cache) {
+                cache[path] = image;
+            }
+
+            return image;
+        }
+    }
+}
---
+++ MediaBrowser/Library/Plugins/RemotePlugin.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MediaBrowser.Library.Plugins {
+    public class RemotePlugin : IPlugin {
+
+        public void Init(MediaBrowser.Library.Kernel kernel) {
+        }
+
+        public string Filename {
+            get;
+            set;
+        }
+
+        public string Name {
+            get;
+            set;
+        }
+
+        public string Description {
+            get;
+            set;
+        }
+
+        public System.Version Version {
+            get;
+            set;
+        }
+
+        public System.Version RequiredMBVersion
+        {
+            get;
+            set;
+        }
+
+        public string BaseUrl
+        {
+            get;
+            set;
+        }
+
+        public virtual bool IsConfigurable
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public virtual void Configure()
+        {
+        }
+
+        public virtual bool InstallGlobally
+        {
+            get;
+            set;
+        }
+    }
+}
