08c953c4eb7a31d16b7e66723b0585f9cd97df7f
Sam Saffron <sam.saffron@gmail.com>
2010-02-19 13:38:24 +1100

    New setting for: http://community.mediabrowser.tv/permalinks/508/build-1384-after-file-has-been-played-mediabrowser-behaves-sluggishly, CacheAllImagesInMemory default true, when set to false MB will consume significantly less memory Cleaned up threading

---
+++ MediaInfoProvider/Plugin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MediaBrowser.Library.Plugins;
+using MediaBrowser.Library;
+using MediaBrowser.Library.Logging;
+
+namespace MediaInfoProvider {
+    public class Plugin : BasePlugin {
+
+        internal const string PluginName = "MediaInfo Provider";
+        internal const string PluginDescription = "This plugin provides rich information about your media using the MediaInfo project."; 
+
+        public override void Init(Kernel kernel) {
+            kernel.MetadataProviderFactories.Add(MetadataProviderFactory.Get<MediaInfoProvider>()); 
+        }
+
+        public override string Name {
+            get { return PluginName; }
+        }
+
+        public override string Description {
+            get { return PluginDescription; }
+        }
+    }
+}
