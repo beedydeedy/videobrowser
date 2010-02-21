87539e5403768f243e44adb1d9d86e8442dd186e
Sam Saffron <sam.saffron@gmail.com>
2010-02-22 09:59:58 +1100

    clean up flickering of default image in poster

---
+++ DvrmsMetadataProvider/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MediaBrowser.Library.Plugins;
+using MediaBrowser.Library;
+
+namespace DvrmsMetadataProvider {
+    public class Plugin : BasePlugin {
+
+        internal const string PluginName = "DVR-MS metadata";
+        internal const string PluginDescription = "This plugin provides metadata for DVR-MS files. (all your recorded tv shows start off as dvr-ms files)"; 
+
+        public override void Init(Kernel kernel) {
+            kernel.MetadataProviderFactories.Add(new MetadataProviderFactory(typeof(DvrmsMetadataProvider))); 
+        }
+
+        public override string Name {
+            get { return PluginName; }
+        }
+
+        public override string Description {
+            get { return PluginDescription; }
+        }
+    }
+}
