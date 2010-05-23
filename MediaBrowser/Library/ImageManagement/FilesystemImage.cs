381e19295d1eecce78b16ba612de0b975be1cf1f
Sam Saffron <sam.saffron@gmail.com>
2010-05-23 19:27:06 +1000

    Fix: http://community.mediabrowser.tv/permalinks/2510/cronos---upgrade-causes-cache-corruption
    Added a new confirmation of the deserialized size in the store, if for any reason the size does not match an exception will be raised.

---
+++ MediaBrowser/Library/ImageManagement/FilesystemImage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using MediaBrowser.Library.Filesystem;
+using MediaBrowser.Library.Persistance;
+using System.Drawing;
+
+namespace MediaBrowser.Library.ImageManagement {
+    public class FilesystemImage : LibraryImage{
+
+
+
+        /*
+         bool imageIsCached;
+        public override void Init() {
+            base.Init();
+
+            imageIsCached = System.IO.Path.GetPathRoot(this.Path).ToLower() != System.IO.Path.GetPathRoot(cachePath).ToLower();
+
+        }*/
+
+
+        protected override bool ImageOutOfDate(DateTime date) {
+            var info = new System.IO.FileInfo(Path);
+            return date < Max(info.CreationTimeUtc, info.LastWriteTimeUtc);
+        }
+
+        protected override System.Drawing.Image OriginalImage {
+            get {
+                return Image.FromFile(Path);
+            }
+        }
+
+        private static DateTime Max(DateTime first, DateTime second) {
+            if (first > second) return first;
+            return second;
+        } 
+    
+    }
+}
