2f0c07b6f426913258d1b2e138e36386ded3e4f6
chris p chipmonkery <chris p chipmonkery@gmail.com>
2010-07-26 11:54:47 +0100

    Add new aspect ratio icons Add new audio icons Rename some Dolby icons to maintain naming convention with DTS Replace some mediainfo icons with new versions Tidy up code in images.mcml

---
+++ MediaBrowser/Library/ImageManagement/FilesystemImage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using MediaBrowser.Library.Filesystem;
+using MediaBrowser.Library.Persistance;
+using System.Drawing;
+
+namespace MediaBrowser.Library.ImageManagement {
+    public class FilesystemImage : LibraryImage{
+
+
+
+        /*
+         bool imageIsCached;
+        public override void Init() {
+            base.Init();
+
+            imageIsCached = System.IO.Path.GetPathRoot(this.Path).ToLower() != System.IO.Path.GetPathRoot(cachePath).ToLower();
+
+        }*/
+
+
+        protected override bool ImageOutOfDate(DateTime date) {
+            var info = new System.IO.FileInfo(Path);
+            //make sure we have valid date info because some systems seem to have troubles with this
+            DateTime now = DateTime.UtcNow;
+            if (info.CreationTimeUtc > now || info.LastWriteTimeUtc > now || info.CreationTimeUtc > info.LastWriteTimeUtc)
+            {
+                //something goofy with these dates...
+                MediaBrowser.Library.Logging.Logger.ReportInfo("Bad date info for image "+Path+". Create date: " + info.CreationTimeUtc + " Mod date: " + info.LastWriteTimeUtc);
+                return false;
+            }
+            return date < Max(info.CreationTimeUtc, info.LastWriteTimeUtc);
+        }
+
+        protected override System.Drawing.Image OriginalImage {
+            get {
+                return Image.FromFile(Path);
+            }
+        }
+
+        private static DateTime Max(DateTime first, DateTime second) {
+            if (first > second) return first;
+            return second;
+        } 
+    
+    }
+}
---
+++ MediaBrowser/Library/MediaType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MediaBrowser.Library {
+    public enum MediaType { 
+        Unknown,
+        Mpg,
+        Avi,
+        Mkv,
+        Mp4,
+        DVD,
+        BluRay,
+        HDDVD,
+        TS,
+        ISO,
+        PlayList,
+        DVRMS,
+        Wmv,
+        Ogv,
+        Mov,
+        Asf,
+        F4v,
+        Flv,
+        G3p
+    }
+}
