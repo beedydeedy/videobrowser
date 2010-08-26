5cb192b1e081b2b4baa980dc6e4e977cf7bbf2d0
Birkoff <Birkoff@localhost>
2010-08-26 18:15:41 +0200

    fix for: http://community.mediabrowser.tv/permalinks/3559/change-wording-on-configuration-wizard-to-be-more-consistent

---
+++ NndbMetadataProvider/Plugin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MediaBrowser.Library.Plugins;
+using MediaBrowser.Library.Logging;
+using MediaBrowser.Library;
+
+namespace NndbMetadataProvider {
+    class Plugin : BasePlugin {
+
+        internal const string PluginName = "Nndb image provider";
+        internal const string PluginDescription = "Downloads actor and director images from nndb.com";
+
+
+        public override void Init(Kernel kernel) {
+            kernel.MetadataProviderFactories.Add(MetadataProviderFactory.Get<NndbPeopleProvider>());
+        }
+
+        public override string Name {
+            get { return PluginName; }
+        }
+
+        public override string Description {
+            get { return PluginDescription; }
+        }
+    }
+}
