f06e9e5faee4b79a73190830fff36a9b2de9e5bf
Birkoff <Birkoff@localhost>
2010-11-16 17:12:58 +0100

    MediaInfo Provider: minor change to description to show included mediainfo.dll version

---
+++ MediaInfoProvider/Plugin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MediaBrowser.Library.Plugins;
+using MediaBrowser.Library;
+using MediaBrowser.Library.Logging;
+
+namespace MediaInfoProvider {
+    public class Plugin : BasePlugin {
+
+        internal const string PluginName = "MediaInfo Provider";
+        internal const string PluginDescription = "This plugin provides rich information about your media using the MediaInfo project.\n\nThis version includes MediaInfo.dll version " + Plugin.includedMediaInfoDLL + "."; 
+        internal const string includedMediaInfoDLL = "0.7.36.0";
+
+        public override void Init(Kernel kernel) {
+            kernel.MetadataProviderFactories.Add(MetadataProviderFactory.Get<MediaInfoProvider>()); 
+        }
+
+        public override string Name {
+            get { return PluginName; }
+        }
+
+        public override string Description {
+            get { return PluginDescription; }
+        }
+        public override System.Version RequiredMBVersion
+        {
+            get
+            {
+                return new System.Version(2, 2, 6, 0);
+            }
+        }
+        public override System.Version TestedMBVersion
+        {
+            get
+            {
+                return new System.Version(2, 2, 3, 0);
+            }
+        }
+    }
+}
