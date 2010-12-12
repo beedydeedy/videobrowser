da98b7fde89868fd512267a72d2c9d6a18870777
Sam Saffron <sam@stackoverflow.com>
2010-12-12 13:12:07 +1100

    Media Browser service v1

---
+++ MediaBrowser/Library/Plugins/IPlugin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MediaBrowser.Library.Entities;
+using MediaBrowser.Library.Factories;
+
+namespace MediaBrowser.Library.Plugins {
+    /// <summary>
+    /// This interface can be implemented by plugin to provide rich information about the plugin
+    ///  It also provides plugins with a place to place initialization code
+    /// </summary>
+    public interface IPlugin {
+        void Init(Kernel kernel);
+        string Filename { get; }
+        string Name { get; }
+        string Description { get; }
+        string RichDescURL { get; }
+        System.Version Version { get; }
+        System.Version RequiredMBVersion { get; }
+        System.Version TestedMBVersion { get; }
+        bool IsConfigurable { get; }
+        void Configure();
+        bool InstallGlobally { get; }
+        /// <summary>
+        /// Plugin is only to be initialized if it is running in the service
+        /// </summary>
+        bool ServiceOnly { get; }
+    }
+}
---
+++ MediaBrowser/Library/Plugins/RemotePlugin.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MediaBrowser.Library.Plugins {
+    public class RemotePlugin : IPlugin {
+
+        public void Init(MediaBrowser.Library.Kernel kernel) {
+        }
+
+        public string Filename {
+            get;
+            set;
+        }
+
+        public string Name {
+            get;
+            set;
+        }
+
+        public string Description {
+            get;
+            set;
+        }
+
+        public string RichDescURL
+        {
+            get;
+            set;
+        }
+        public System.Version Version
+        {
+            get;
+            set;
+        }
+
+        public System.Version RequiredMBVersion
+        {
+            get;
+            set;
+        }
+
+        public System.Version TestedMBVersion
+        {
+            get;
+            set;
+        }
+
+        public string BaseUrl
+        {
+            get;
+            set;
+        }
+
+        public virtual bool IsConfigurable
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public virtual void Configure()
+        {
+        }
+
+        public virtual bool InstallGlobally
+        {
+            get;
+            set;
+        }
+
+        public virtual bool ServiceOnly { get; set; }
+    }
+}
