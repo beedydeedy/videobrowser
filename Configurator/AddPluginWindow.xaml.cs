c6571b9243850c2281d1aebd59d6fd8c2bd5483b
Sam Saffron <sam.saffron@gmail.com>
2011-01-03 22:38:07 +1100

    Plumbing for media browser web service / web front end

---
+++ Configurator/AddPluginWindow.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Configurator.Code;
+using MediaBrowser.Library.Plugins;
+using MediaBrowser.Library.Threading;
+using MediaBrowser.Library;
+using System.Windows.Forms;
+using System.Threading;
+using System.Windows.Threading;
+using MediaBrowser.Library.Logging;
+
+namespace Configurator {
+    /// <summary>
+    /// Interaction logic for AddPluginWindow.xaml
+    /// </summary>
+    public partial class AddPluginWindow : Window {
+        public AddPluginWindow() {
+            InitializeComponent();
+            progress.Minimum = 0;
+            progress.Maximum = 100;
+            pluginList_SelectionChanged(null, null); //make sure first description loads
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e) {
+            PluginSourcesWindow window = new PluginSourcesWindow();
+            window.ShowDialog();
+        }
+
+        private void InstallClick(object sender, RoutedEventArgs e) {
+            InstallButton.IsEnabled = false;
+            btnDone.IsEnabled = false;
+            pluginList.IsEnabled = false;
+            this.progress.Visibility = Visibility.Visible;
+            PluginInstaller p = new PluginInstaller();
+            callBack done = new callBack(InstallFinished);
+            p.InstallPlugin(pluginList.SelectedItem as IPlugin, progress, this, done);
+            MainWindow.Instance.KernelModified = true;
+
+        }
+
+        private delegate void callBack();
+
+        public void InstallFinished()
+        {
+            //called when the install is finished - we want to close
+            //don't close anymore - leave open for another plugin
+            //this.Close();
+            InstallButton.IsEnabled = true;
+            btnDone.IsEnabled = true;
+            pluginList.IsEnabled = true;
+            this.progress.Value = 0;
+            this.progress.Visibility = Visibility.Hidden;
+            IPlugin plugin = pluginList.SelectedItem as IPlugin;
+            if (plugin != null)
+            {
+                updateAttributes(plugin);
+                Logger.ReportInfo(plugin.Name + " v" + plugin.Version + " Installed.");
+            }
+        }
+
+        private void updateAttributes(IPlugin plugin)
+        {
+            PluginManager.Instance.UpdateAvailableAttributes(plugin, true);
+            pluginList.Items.Refresh();
+        }
+
+        private void btnDone_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pluginList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //validate the required MB version for this plug-in
+            if (pluginList.SelectedItem != null && InstallButton != null && MessageLine != null)
+            {
+                IPlugin plugin = pluginList.SelectedItem as IPlugin;
+                if (plugin.RequiredMBVersion > Kernel.Instance.Version)
+                {
+                    InstallButton.IsEnabled = false;
+                    MessageLine.Content = plugin.Name + " requires at least version " + plugin.RequiredMBVersion + ".  Current MB version installed is " + Kernel.Instance.Version;
+                }
+                else
+                {
+                    InstallButton.IsEnabled = true;
+                    MessageLine.Content = "";
+                }
+                if (RichDescFrame != null)
+                {
+                    if (!String.IsNullOrEmpty(plugin.RichDescURL))
+                    {
+                            RichDescFrame.Navigate(new Uri(plugin.RichDescURL, UriKind.Absolute));
+                            RichDescFrame.Visibility = Visibility.Visible;
+                    }
+                    else
+                    {
+                        RichDescFrame.Visibility = Visibility.Hidden;
+                    }
+                }
+                    
+            }
+        }
+
+        private void RichDescFrame_NavigationFailed(object sender, System.Windows.Navigation.NavigationFailedEventArgs e)
+        {
+            Logger.ReportError("Navigation to Rich Description failed.  Error: "+e.Exception.Message);
+            RichDescFrame.Visibility = Visibility.Hidden;
+            e.Handled = true;
+        }
+
+        private void RichDescFrame_Navigating(object sender, System.Windows.Navigation.NavigatingCancelEventArgs e)
+        {
+            if (pluginList.SelectedItem != null)
+            {
+                IPlugin plugin = pluginList.SelectedItem as IPlugin;
+                if (e.Uri != new Uri(plugin.RichDescURL, UriKind.Absolute))
+                {
+                    MessageLine.Content = "Cannot Follow Links";
+                    e.Cancel = true; //don't allow navigating away from our main page
+                }
+            }
+        }
+
+        private void RichDescFrame_LoadCompleted(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+            mshtml.HTMLDocumentClass doc = (mshtml.HTMLDocumentClass)RichDescFrame.Document;
+            if (doc.body.innerHTML.Contains("404:"))
+            {
+                Logger.ReportError("Rich Description Not Found.");
+                RichDescFrame.Visibility = Visibility.Hidden;
+            }            
+        }
+
+
+    }
+}
---
+++ FilmTrailerPlugin/PluginOptions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MediaBrowser.Library.Plugins.Configuration;
+using MediaBrowser.Library.Plugins;
+
+namespace FilmTrailerPlugin {
+    public class PluginOptions : PluginConfigurationOptions {
+        [Label("Menu Name:")]
+        public string MenuName = "Film Trailers";
+        [Label("Refresh Interval (hours):")]
+        public string RefreshIntervalHrs = "24";
+
+        [Label("Trailer Source Country:")]
+        [Items("Australia,Denmark,Finland,France,Germany,Italy,Spain,Sweden,Switzerland,Switzerland (fr),The Netherlands,United Kingdom,** CUSTOM **")]
+        public string TrailerSource = "United Kingdom";
+
+        [Label("Custom (Advanced Only):")]
+        public string TrailerSourceCustom = FilmTrailerFolder.FeedDefault;
+    }
+}
