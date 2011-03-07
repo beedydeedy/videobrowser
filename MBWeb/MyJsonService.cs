ab5ce8c7b1ad65cc745edf824e1a72b665a890b3
Eric <ebr@localhost>
2011-03-07 11:59:16 -0500

    use mutex to block during initialization - works around a crash when two intstances are started in rapid succession

---
+++ MBWeb/MyJsonService.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MediaBrowser.Library;
+using MediaBrowser.Library.Entities;
+using MediaBrowser.Web.Framework;
+using MediaBrowser.Library.Persistance;
+using System.Collections;
+
+namespace MediaBrowser
+{
+
+    public class MyJsonService: MediaBrowser.Web.Framework.JsonService
+    {
+        static Dictionary<Guid, BaseItem> ItemMap = new Dictionary<Guid, BaseItem>();
+
+        [Route("/item")]
+        public object Item(Guid? id)
+        {
+            Folder folder = null;
+            BaseItem item = null;
+
+            if (id == null)
+            {
+                folder = Kernel.Instance.RootFolder;
+            }
+
+            if (folder == null)
+            {
+                lock (ItemMap)
+                {
+                    item = ItemMap[id.Value];
+                }
+                folder = item as Folder;
+            }
+  
+
+            if (folder != null)
+            {
+                List<Dictionary<string, object>> result = new List<Dictionary<string, object>>();
+                foreach (var child in folder.Children)
+                {
+                    var row = ToJsonSerializable(child)  as Dictionary<string, object>;
+                    row["Name"] = child.Name;
+                    row["Id"] = child.Id;
+                    result.Add(row);
+                }
+
+                lock (ItemMap)
+                {
+                    foreach (var baseItem in folder.Children)
+                    {
+                        ItemMap[baseItem.Id] = baseItem;
+                    }
+                }
+
+
+                return result;
+            }
+           
+
+            return new object[] { item };
+        }
+
+
+        public object ToJsonSerializable(object input)
+        {
+            Type type = input != null ? input.GetType() : (Type)null;
+            if (input == null || type.IsValueType || type.IsEnum || type == typeof(string))
+            {
+                return input;
+            }
+
+            if (typeof(IList).IsAssignableFrom(type))
+            {
+                var list = new List<object>();
+                foreach (var item in (IList)input)
+                {
+                    list.Add(item);
+                }
+                return list;
+            }
+
+            var rval = new Dictionary<string, object>();
+            foreach (var persistable in Serializer.GetPersistables(input))
+            {
+                rval[persistable.MemberInfo.Name] = ToJsonSerializable(persistable.GetValue(input));
+            }
+            return rval;
+        }
+    }
+}
---
+++ MediaBrowser/Library/Interfaces/IItemRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediaBrowser.Library.Entities;
+using MediaBrowser.Library.Interfaces;
+
+namespace MediaBrowser.Library
+{
+    public interface IItemRepository
+    {
+        IEnumerable<IMetadataProvider> RetrieveProviders(Guid guid);
+        void SaveProviders(Guid guid, IEnumerable<IMetadataProvider> providers);
+
+        void SaveItem(BaseItem item);
+        BaseItem RetrieveItem(Guid name);
+        void SaveChildren(Guid ownerName, IEnumerable<Guid> children);
+        IEnumerable<Guid> RetrieveChildren(Guid id);
+
+        PlaybackStatus RetrievePlayState(Guid id);
+        DisplayPreferences RetrieveDisplayPreferences(Guid id);
+        ThumbSize RetrieveThumbSize(Guid id);
+        
+        void SavePlayState( PlaybackStatus playState);
+        void SaveDisplayPreferences(DisplayPreferences prefs);
+        void ShutdownWriter();
+
+        bool ClearEntireCache();
+        
+    }
+}
