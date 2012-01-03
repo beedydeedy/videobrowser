b0f13c4bfe3a2cc0fed429af228f43ce42364d05
Eric <ebr@localhost>
2012-01-03 13:39:47 -0500

    encapsulate series retrieval in episode, attempt to handle missing runtimes on episodes

---
+++ MediaBrowser/Library/Entities/Media.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MediaBrowser.Library.Entities {
+    public abstract class Media : BaseItem{
+        PlaybackStatus PlayState {get; set; }
+        protected PlaybackStatus playbackStatus;
+        public virtual PlaybackStatus PlaybackStatus { get { return playbackStatus; } }
+        public abstract IEnumerable<string> Files {get;}
+
+        public override bool PlayAction(Item item)
+        {
+            Application.CurrentInstance.Play(item, false, false); //play with no intros
+            return true;
+        }
+
+        public bool IsPlaylistCapable()
+        {
+            Video us = this as Video;
+            if (us != null)
+            {
+                return !us.ContainsRippedMedia;
+            }
+            return true;
+        }
+
+        public virtual int RunTime
+        {
+            get { return 0; }
+        }
+    }
+}
