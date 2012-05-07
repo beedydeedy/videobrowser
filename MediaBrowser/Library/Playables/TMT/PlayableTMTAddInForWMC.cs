54afb054e8fb9ccd3cc0ed23f91ee68bb5902c29
Eric <ebr@localhost>
2012-05-07 09:51:17 -0400

    hide metadata tab on help

---
+++ MediaBrowser/Library/Playables/TMT/PlayableTMTAddInForWMC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MediaBrowser.Library.Playables.TMT
+{
+    /// <summary>
+    /// Represents an external player that uses the WMC add-in
+    /// </summary>
+    public class PlayableTMTAddInForWMC : PlayableTMT
+    {
+        protected override Type PlaybackControllerType
+        {
+            get
+            {
+                return typeof(TMTAddInPlaybackController);
+            }
+        }
+    }
+}
