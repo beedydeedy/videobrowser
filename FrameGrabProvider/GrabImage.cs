5fa6d8ba93edc80f1064b0d7a94288fe5d58d6f0
luke <luke@luke-server.esr9850>
2012-05-14 21:19:12 -0400

    Merge

---
+++ FrameGrabProvider/GrabImage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Drawing;
+using MediaBrowser.Library.ImageManagement;
+using MediaBrowser.Library.Logging;
+using MediaBrowser.Library.Entities;
+
+namespace FrameGrabProvider {
+    class GrabImage : FilesystemImage {
+
+        protected override bool ImageOutOfDate(DateTime date) {
+            return false;
+        }
+
+        protected override Image OriginalImage {
+            get {
+                string video = this.Path.Substring(7);
+
+                Logger.ReportInfo("Trying to extract thumbnail for " + video);
+
+                string localFilename = System.IO.Path.GetTempFileName() + ".png";
+
+                if (ThumbCreator.CreateThumb(video, localFilename, 0.2)) {
+                    if (File.Exists(localFilename)) {
+                        //load image and pass to processor
+                        return Image.FromFile(localFilename);
+                    }
+                } 
+                
+                Logger.ReportWarning("Failed to grab thumbnail for " + video);
+                return null;
+            }
+        }
+
+    }
+}
---
+++ MusicPlugin/Code/ModelItems/PlaybackControllerMusic.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.MediaCenter.UI;
+using Microsoft.MediaCenter.Hosting;
+using Microsoft.MediaCenter;
+using MediaBrowser.Library.RemoteControl;
+using MediaBrowser.LibraryManagement;
+using MediaBrowser;
+using MusicPlugin.LibraryManagement;
+
+
+namespace MusicPlugin
+{
+
+    public class PlaybackControllerMusic : PlaybackController, IPlaybackController
+    {
+        public PlaybackControllerMusic()
+            : base()
+        { }
+
+        #region IPlaybackController Members
+
+        public override bool CanPlay(string filename)
+        {
+            if (filename == null)
+                return false;
+            return MusicHelper.IsMusic(filename);
+        }
+
+        public override bool CanPlay(IEnumerable<string> files)
+        {
+            if (files == null)
+                return false;
+
+            foreach (string file in files)
+                if (CanPlay(file))
+                    return true;
+
+            return false;
+        }
+        #endregion
+
+        protected override bool MonitorMediaTransportPropertyChanged
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
