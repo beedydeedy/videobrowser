573cfde847ff35a08594f19846a22a13d0964b9e
Sam Saffron <sam.saffron@gmail.com>
2012-05-21 15:52:33 +1000

    wow ... this is an old bug, async has had a very nasty bug since inception, in particular adding jobs once there are a bunch of things in the queue never worked right, new design uses a linked list so all the prioritisation stuff works properly

--- MediaBrowser/Library/Threading/Async.cs
+++ MediaBrowser/Library/Threading/Async.cs
@@ -1,48 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Diagnostics;
 using MediaBrowser.Library.Logging;
+using System.Text;
 
-namespace MediaBrowser.Library.Threading {
-
-    public static class Async {
-
+namespace MediaBrowser.Library.Threading 
+{
+    public static class Async
+    {
         public const string STARTUP_QUEUE = "Startup Queue";
 
-        class ThreadPool {
-            List<Action> actions = new List<Action>();
+        class ThreadPool
+        {
+            LinkedList<Action> actions = new LinkedList<Action>();
             List<Thread> threads = new List<Thread>();
             string name;
             volatile int maxThreads = 1;
 
-            public ThreadPool(string name) {
+            public ThreadPool(string name)
+            {
                 Debug.Assert(name != null);
-                if (name == null) {
+                if (name == null)
+                {
                     throw new ArgumentException("name should not be null");
                 }
                 this.name = name;
             }
 
-
-            public void SetMaxThreads(int maxThreads) {
+            public void SetMaxThreads(int maxThreads)
+            {
                 Debug.Assert(maxThreads > 0);
-                if (maxThreads < 1) {
+                if (maxThreads < 1)
+                {
                     throw new ArgumentException("maxThreads should be larger than 0");
                 }
 
                 this.maxThreads = maxThreads;
             }
 
-            public void Queue(Action action, bool urgent) {
+            public void Queue(Action action, bool urgent)
+            {
                 Queue(action, urgent, 0);
             }
 
-            public void Queue(Action action, bool urgent, int delay) {
+            public void Queue(Action action, bool urgent, int delay)
+            {
 
-                if (delay > 0) {
+                if (delay > 0)
+                {
                     Timer t = null;
                     t = new Timer(_ =>
                     {
@@ -52,10 +59,12 @@ namespace MediaBrowser.Library.Threading {
                     return;
                 }
 
-                lock (threads) {
+                lock (threads)
+                {
                     // we are spinning up too many threads
                     // should be fixed 
-                    if (maxThreads > threads.Count) {
+                    if (maxThreads > threads.Count)
+                    {
                         Thread t = new Thread(new ThreadStart(ThreadProc));
                         t.IsBackground = true;
                         // dont affect the UI.
@@ -66,70 +75,97 @@ namespace MediaBrowser.Library.Threading {
                     }
                 }
 
-                lock (actions) {
-                    if (urgent) {
-                        actions.Insert(0, action);
-                    } else {
-                        actions.Add(action);
+                lock (actions)
+                {
+                    if (urgent)
+                    {
+                        actions.AddFirst(action);
+                    }
+                    else
+                    {
+                        actions.AddLast(action);
                     }
 
                     Monitor.Pulse(actions);
                 }
             }
 
-            private void ThreadProc() {
+            private void ThreadProc()
+            {
 
-                while (true) {
+                while (true)
+                {
 
-                    lock (threads) {
-                        if (maxThreads < threads.Count) {
+                    lock (threads)
+                    {
+                        if (maxThreads < threads.Count)
+                        {
                             threads.Remove(Thread.CurrentThread);
                             break;
                         }
                     }
 
-                    List<Action> copy;
+                    Action action;
 
-                    lock (actions) {
-                        while (actions.Count == 0) {
+                    lock (actions)
+                    {
+                        while (actions.Count == 0)
+                        {
                             Monitor.Wait(actions);
                         }
-                        copy = new List<Action>(actions);
-                        actions.Clear();
+                        action = actions.First.Value;
+                        actions.RemoveFirst();
                     }
 
-                    foreach (var action in copy) {
-                        action();
+                    action();
+                }
+            }
+
+            public string Diagnostics
+            {
+                get
+                {
+                    var builder = new StringBuilder();
+                    builder.AppendFormat(" >> maxThreads: {0}\n", maxThreads);
+                    lock (threads)
+                    {
+                        builder.AppendFormat(" >> Total threads: {0}\n", threads.Count);
                     }
+                    return builder.ToString();
                 }
             }
         }
 
-
         static Dictionary<string, ThreadPool> threadPool = new Dictionary<string, ThreadPool>();
 
-        public static Timer Every(int milliseconds, Action action) {
+        public static Timer Every(int milliseconds, Action action)
+        {
             Timer timer = new Timer(_ => action(), null, 0, milliseconds);
             return timer;
         }
 
-        public static void SetMaxThreads(string uniqueId, int threads) {
+        public static void SetMaxThreads(string uniqueId, int threads)
+        {
             GetThreadPool(uniqueId).SetMaxThreads(threads);
         }
 
-        public static void Queue(string uniqueId, Action action) {
+        public static void Queue(string uniqueId, Action action)
+        {
             Queue(uniqueId, action, null);
         }
 
-        public static void Queue(string uniqueId, Action action, int delay) {
-            Queue(uniqueId, action, null,false, delay);
+        public static void Queue(string uniqueId, Action action, int delay)
+        {
+            Queue(uniqueId, action, null, false, delay);
         }
 
-        public static void Queue(string uniqueId, Action action, Action done) {
+        public static void Queue(string uniqueId, Action action, Action done)
+        {
             Queue(uniqueId, action, done, false);
         }
 
-        public static void Queue(string uniqueId, Action action, Action done, bool urgent) {
+        public static void Queue(string uniqueId, Action action, Action done, bool urgent)
+        {
             Queue(uniqueId, action, done, urgent, 0);
         }
 
@@ -141,18 +177,23 @@ namespace MediaBrowser.Library.Threading {
         /// <param name="done"></param>
         /// <param name="urgent"></param>
         /// <param name="delay">Millisecond delay before executing </param>
-        public static void Queue(string uniqueId, Action action, Action done, bool urgent, int delay) {
+        public static void Queue(string uniqueId, Action action, Action done, bool urgent, int delay)
+        {
 
             Debug.Assert(uniqueId != null);
             Debug.Assert(action != null);
 
             Action workItem = () =>
             {
-                try {
+                try
+                {
                     action();
-                } catch (ThreadAbortException) { /* dont report on this, its normal */ } catch (Exception ex) {
-                    Debug.Assert(false, "Async thread threw the following exception: " + ex.ToString());
+                }
+                catch (ThreadAbortException) { /* dont report on this, its normal */ }
+                catch (Exception ex)
+                {
                     Logger.ReportException("Async thread threw the following exception: ", ex);
+                    Debug.Assert(false, "Async thread threw the following exception: " + ex.ToString());
                 }
                 if (done != null) done();
             };
@@ -160,16 +201,20 @@ namespace MediaBrowser.Library.Threading {
             GetThreadPool(uniqueId).Queue(workItem, urgent, delay);
         }
 
-        private static ThreadPool GetThreadPool(string uniqueId) {
+        private static ThreadPool GetThreadPool(string uniqueId)
+        {
             ThreadPool currentPool;
-            lock (threadPool) {
-                if (!threadPool.TryGetValue(uniqueId, out currentPool)) {
+            lock (threadPool)
+            {
+                if (!threadPool.TryGetValue(uniqueId, out currentPool))
+                {
                     currentPool = new ThreadPool(uniqueId);
                     threadPool[uniqueId] = currentPool;
                 }
             }
             return currentPool;
         }
+
 
         /// <summary>
         /// Run an action with given timeout - WON"T Stop the action but will return
@@ -184,8 +229,22 @@ namespace MediaBrowser.Library.Threading {
             else
                 throw new TimeoutException("Operation failed to complete in " + timeout / 1000 + " seconds.");
         }
-    
 
+        public static object Diagnostics
+        {
+            get
+            {
+                var builder = new StringBuilder();
+                lock (threadPool)
+                {
+                    foreach (var pool in threadPool)
+                    {
+                        builder.AppendFormat("{0}: \n{1}\n\n", pool.Key, pool.Value.Diagnostics);
+                    }
+                }
+                return builder.ToString();
+            }
+
+        }
     }
-   
 }
