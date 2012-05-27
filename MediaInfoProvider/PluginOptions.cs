0ebd3f8296c00a5a6d04d6b8ea08e80f16b36a0f
luke <luke@luke-server.esr9850>
2012-05-27 10:28:01 -0400

    Don't play Intros if the PlayableItem doesn't have Video

---
+++ MediaInfoProvider/PluginOptions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MediaBrowser.Library.Plugins;
+using MediaBrowser.Library;
+using MediaBrowser.Library.Plugins.Configuration;
+
+namespace MediaInfoProvider
+{
+    public class PluginOptions : PluginConfigurationOptions
+    {
+        [Label("Allow Rips  (service)")]
+        public bool AllowBDRips = false;
+
+        [Label("Timeout (ms)")]
+        public string ServiceTimeout = "60000";
+
+        [Label("Clear Bad File List")]
+        public bool ClearBadFiles = false;
+
+        public List<string> BadFiles = new List<string>();
+        public List<string> FormerBadFiles = new List<string>();
+
+    }
+}
