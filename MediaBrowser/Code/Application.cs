0583e924e25a2143357998fea58fbb27442ce3df
Eric <ebr@localhost>
2012-06-07 10:43:04 -0400

    remove stats ping

--- MediaBrowser/Code/Application.cs
+++ MediaBrowser/Code/Application.cs
@@ -752,8 +752,8 @@ namespace MediaBrowser
             Logger.ReportInfo("*** Internet Providers are "+(Config.AllowInternetMetadataProviders ? "ON." : "OFF."));
             Logger.ReportInfo("*** Theme in use is: " + Config.ViewTheme);
             // Now let's put a diagnostic ping in here for the beta cycle so we can see how much testing we're getting
-            string info = "IP=" + Config.AllowInternetMetadataProviders + " EXTP=" + Config.ExternalPlayers.Count + " EXT=" + RunningOnExtender;
-            Helper.Ping("http://www.ebrsoft.com/software/mb/plugins/ping.php?product=MBBeta&ver=" + Kernel.Instance.VersionStr + "&mac=" + Helper.GetMACAddress() + "&key="+info);
+            //string info = "IP=" + Config.AllowInternetMetadataProviders + " EXTP=" + Config.ExternalPlayers.Count + " EXT=" + RunningOnExtender;
+            //Helper.Ping("http://www.ebrsoft.com/software/mb/plugins/ping.php?product=MBBeta&ver=" + Kernel.Instance.VersionStr + "&mac=" + Helper.GetMACAddress() + "&key="+info);
             try
             {
                 if (Config.IsFirstRun)
