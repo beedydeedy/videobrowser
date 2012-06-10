3de6bf553bc5eba761466c37fa666380a7ce624d
luke <luke@luke-server.esr9850>
2012-06-10 11:01:25 -0400

    Fixed transcoding with dvd's

---
+++ MusicPlugin/Library/Playables/PlayableMultiFileMusic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MediaBrowser.Library.Entities;
+using MediaBrowser.Library.Playables;
+using MusicPlugin.Library.Entities;
+
+namespace MusicPlugin.Library.Playables
+{
+    class PlayableMultiFileMusic : PlayableMultiMediaVideo
+    {
+        public override bool CanPlay(IEnumerable<Media> mediaList)
+        {
+            return mediaList.First() as Music != null;
+        }
+    }
+}
