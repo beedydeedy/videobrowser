86cc1fbfe45834f6e164cb3ee99a087c5e9550f4
luke <luke@luke-server>
2012-07-15 16:35:30 -0400

    Merge

---
+++ DiamondTheme/Code/AppUtil.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using Microsoft.MediaCenter.UI;
+using MediaBrowser.Library;
+
+namespace Diamond
+{
+    public class AppUtil : ModelItem
+    {
+        public AppUtil()
+        {
+        }
+
+        public string formatFirstAirDate(string airDate)
+        {
+            //DateTime dt = new DateTime();
+            return DateTime.Parse(airDate).ToString("dd MMMM yyyy");
+            //return dt.ToString("dd MMMM yyyy");
+        }
+
+    }
+}
+
