74a98814209fc638b7a970a8e81f6512412b63ce
Eric <ebr@localhost>
2012-07-16 19:15:36 -0400

    clear local meta file on forced refresh

---
+++ MBTrailers/MovieTrailer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MediaBrowser.Library.Entities;
+using MediaBrowser.Library.Metadata;
+using MediaBrowser.Library.Persistance;
+using MediaBrowser.Library.Entities.Attributes;
+using MediaBrowser.Library;
+
+namespace MBTrailers
+{
+    class MovieTrailer : Movie
+    {
+        [Persist]
+        [NotSourcedFromProvider]
+        public Guid RealMovieID;
+        private Movie RealMovie;
+
+        public override bool CanResume
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public override bool RefreshMetadata(MediaBrowser.Library.Metadata.MetadataRefreshOptions options)
+        {
+            if (RealMovie == null) RealMovie = (Movie)Kernel.Instance.ItemRepository.RetrieveItem(RealMovieID);
+            MediaBrowser.Library.Logging.Logger.ReportInfo("Refreshing trailer for " + RealMovie.Name);
+            this.PrimaryImagePath = Util.CloneImage(RealMovie.PrimaryImagePath);
+            this.BackdropImagePaths = new List<string>();
+            if (RealMovie.BackdropImagePaths != null)
+            {
+                foreach (string backdrop in RealMovie.BackdropImagePaths)
+                {
+                    this.BackdropImagePaths.Add(backdrop);
+                }
+            }
+            if ((options & MetadataRefreshOptions.Force) == MetadataRefreshOptions.Force)
+            {
+                var images = new List<MediaBrowser.Library.ImageManagement.LibraryImage>();
+                images.Add(PrimaryImage);
+                images.AddRange(BackdropImages);
+
+                foreach (var image in images)
+                {
+                    try
+                    {
+                        if (image != null)
+                        {
+                            image.ClearLocalImages();
+                            MediaBrowser.Library.Factories.LibraryImageFactory.Instance.ClearCache(image.Path);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MediaBrowser.Library.Logging.Logger.ReportException("Failed to clear local image (its probably in use)", ex);
+                    }
+                }
+                //changed = RealMovie.RefreshMetadata(options);
+            }
+
+        
+            this.Overview = RealMovie.Overview;
+            this.ProductionYear = RealMovie.ProductionYear;
+            this.RunningTime = RealMovie.RunningTime;
+            this.Actors = RealMovie.Actors;
+            this.Directors = RealMovie.Directors;
+            this.Name = RealMovie.Name + " " + Plugin.PluginOptions.Instance.TrailerSuffix;
+            this.SortName = RealMovie.SortName;
+            this.Studios = RealMovie.Studios;
+            this.Path = RealMovie.TrailerFiles.First();
+            this.MediaType = MediaTypeResolver.DetermineType(Path);
+            this.MediaInfo = RealMovie.MediaInfo;
+            this.DateCreated = System.IO.File.GetCreationTime(this.Path);
+            this.DisplayMediaType = "Trailer";
+            Plugin.proxy.SetTrailerInfo(this);
+            Kernel.Instance.ItemRepository.SaveItem(this);
+
+            return true;
+        }
+
+        
+    }
+}
---
+++ MediaBrowser/Library/Providers/ImageByNameProvider.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediaBrowser.LibraryManagement;
+using System.IO;
+using MediaBrowser.Library.Providers.Attributes;
+using MediaBrowser.Library.Entities;
+using MediaBrowser.Library.Configuration;
+
+namespace MediaBrowser.Library.Providers
+{
+    [ProviderPriority(20)]
+    [SupportedType(typeof(BaseItem))]
+    class ImageByNameProvider : ImageFromMediaLocationProvider
+    {
+        protected string location;
+        protected override string Location
+        {
+            get {
+                if (location == null)
+                {
+
+                    location = ApplicationPaths.AppIBNPath;
+
+                    //sub-folder is based on the type of thing we're looking for
+                    if (Item is Genre)
+                            location = Path.Combine(location, "Genre");
+                    else if (Item is Person)
+                            location = Path.Combine(location, "People");
+                    else if (Item is Studio)
+                            location = Path.Combine(location, "Studio");
+                    else if (Item is Year)
+                            location = Path.Combine(location, "Year");
+                    else
+                            location = Path.Combine(location, "General");
+
+
+                    char[] invalid = Path.GetInvalidFileNameChars();
+
+                    string name = Item.Name;
+                    foreach (char c in invalid)
+                        name = name.Replace(c.ToString(), "");
+                    location = Path.Combine(location, name);
+
+                    if (!Directory.Exists(location))
+                    {
+                        //try the default area by type
+                        location = ApplicationPaths.AppIBNPath; //reset to root
+                        location = Path.Combine(location, "Default\\"+Item.GetType().Name);
+
+                        //now we have a specific default folder for this type - be sure it exists
+                        if (!Directory.Exists(location))
+                        {
+                            //nope - use a generic
+                            string baseType = Item is Folder ? "folder" : "video";
+                            location = Path.Combine(ApplicationPaths.AppIBNPath, "default\\" + baseType);
+                        }
+                    }
+                }
+                return location;
+            }
+        }
+
+        
+    }
+}
